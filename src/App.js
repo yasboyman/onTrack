cdcb9461e44dce8d729bf9f41c3781b8b4a2f1ba
Y <y@Ys-MacBook-Air.connect>
2021-11-02 14:00:06 +0000

    project inititialised, post request successful, set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import BooksCard from "./components/BooksCard";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState("");
+  const [currentPage, setcurrentPage] = useState(1);
+  const [SelectedItemsPerPage, setItemsPerPage] = useState(20);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // const handleFetch = async () =>
+    //   await fetch("http://nyx.vima.ekt.gr:3000/api/books/", {
+    //     method: "POST",
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       page: currentPage,
+    //       itemsPerPage: SelectedItemsPerPage,
+    //     }),
+    //   }).then((data) => {
+    //     const result = [data.json()];
+    //     setData(result);
+    //     setIsLoading(false);
+    //     // console.log("data!!", data.json());
+    //   });
+    //
+    // handleFetch();
+
+    const url = "http://nyx.vima.ekt.gr:3000/api/books/";
+
+    const getDevices = async () => {
+      const location = window.location.hostname;
+      const settings = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          page: currentPage,
+          itemsPerPage: SelectedItemsPerPage,
+        }),
+      };
+      try {
+        const fetchResponse = await fetch(`${url}`, settings);
+        const result = await fetchResponse.json();
+        setIsLoading(false);
+        return setData(result);
+      } catch (e) {
+        return e;
+      }
+    };
+    getDevices();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Boooooks fiascooo</h2>
+      {!isLoading &&
+        data &&
+        data.map((item) => {
+          return (
+            <BooksCard
+              // id={item.id}
+              key={item.index}
+              title={item.book_title}
+              author={item.book_author}
+              book_pages={item.book_pages}
+              year={item.book_publication_year}
+            />
+          );
+        })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BooksCard/index.js
@@ -0,0 +1,5 @@
+const BooksCard = ({ title, author, book_pages, year }) => {
+  return <div>{title}</div>;
+};
+
+export default BooksCard;
