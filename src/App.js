a6e9157caac2895013a059f280f0192c3c216235
Y <y@Ys-MacBook-Air.local>
2021-11-04 00:12:07 +0000

    react pagnation added with logic handling, pagnation works

--- src/App.js
+++ src/App.js
@@ -1,79 +1,101 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import BooksCard from "./components/BooksCard";
+import ReactPaginate from "react-paginate";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState("");
   const [isLoading, setIsLoading] = useState("");
-  const [currentPage, setcurrentPage] = useState(1);
-  const [SelectedItemsPerPage, setItemsPerPage] = useState(20);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [SelectedItemsPerPage, setItemsPerPage] = useState(1);
+  const [count, setCount] = useState("");
+
+  const url = "http://nyx.vima.ekt.gr:3000/api/books/";
+
+  const getBooks = async () => {
+    // const location = window.location.hostname;
+    const settings = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        page: currentPage,
+        itemsPerPage: SelectedItemsPerPage,
+      }),
+    };
+    try {
+      const fetchResponse = await fetch(`${url}`, settings);
+      const result = await fetchResponse.json();
+      setIsLoading(false);
+      console.log("RESUTTTTTT", result);
+      setCount(Math.ceil(result.count));
+      return setData([result]);
+    } catch (e) {
+      return e;
+    }
+  };
 
   useEffect(() => {
     setIsLoading(true);
-    // const handleFetch = async () =>
-    //   await fetch("http://nyx.vima.ekt.gr:3000/api/books/", {
-    //     method: "POST",
-    //     headers: {
-    //       Accept: "application/json",
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       page: currentPage,
-    //       itemsPerPage: SelectedItemsPerPage,
-    //     }),
-    //   }).then((data) => {
-    //     const result = [data.json()];
-    //     setData(result);
-    //     setIsLoading(false);
-    //     // console.log("data!!", data.json());
-    //   });
-    //
-    // handleFetch();
-
-    const url = "http://nyx.vima.ekt.gr:3000/api/books/";
-
-    const getDevices = async () => {
-      const location = window.location.hostname;
-      const settings = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          page: currentPage,
-          itemsPerPage: SelectedItemsPerPage,
-        }),
-      };
-      try {
-        const fetchResponse = await fetch(`${url}`, settings);
-        const result = await fetchResponse.json();
-        setIsLoading(false);
-        return setData(result);
-      } catch (e) {
-        return e;
-      }
-    };
-    getDevices();
+    getBooks();
   }, []);
 
   console.log(data);
 
+  const handlePageClick = (event) => {
+    const newOffset = event.selected + 1;
+    console.log(
+      `User requested page number ${event.selected}, which is offset `
+    );
+    setCurrentPage(newOffset);
+    getBooks();
+  };
+
   return (
     <div className="App">
       <h2>Boooooks fiascooo</h2>
+      {!isLoading && (
+        <>
+          {/*//*/}
+          {/*// /!*<div>*!/*/}
+          {/*// /!*  <input*!/*/}
+          {/*// /!*  placeholder={Search}*!/*/}
+          {/*// /!*  />*!/*/}
+          {/*// /!*</div>*!/*/}
+          <ReactPaginate
+            pageCount={count}
+            pageRange={2}
+            forcePage={currentPage - 1}
+            marginPagesDisplayed={2}
+            onPageChange={handlePageClick}
+            containerClassName={"container"}
+            previousLinkClassName={"page"}
+            breakClassName={"page"}
+            nextLinkClassName={"page"}
+            pageClassName={"page"}
+            disabledClassName={"disabled"}
+            activeClassName={"active"}
+          />
+        </>
+      )}
+
       {!isLoading &&
         data &&
-        data.map((item) => {
+        data[0].books.map((item) => {
+          console.log(item);
           return (
-            <BooksCard
-              // id={item.id}
-              key={item.index}
-              title={item.book_title}
-              author={item.book_author}
-              book_pages={item.book_pages}
-              year={item.book_publication_year}
-            />
+            <div className={"books_parent_container"}>
+              <BooksCard
+                // id={item.id}
+                key={item.index}
+                title={item.book_title}
+                author={item.book_author}
+                book_pages={item.book_pages}
+                year={item.book_publication_year}
+              />
+            </div>
           );
         })}
     </div>
--- src/components/BooksCard/index.js
+++ src/components/BooksCard/index.js
@@ -1,5 +1,26 @@
+import React from "react";
+import bookBackground from "../../assets/booksCard/index.png";
+import classes from "./index.module.css";
+import { BsVectorPen } from "react-icons/bs";
+
 const BooksCard = ({ title, author, book_pages, year }) => {
-  return <div>{title}</div>;
+  return (
+    <>
+      <div className={classes.books_container}>
+        <img src={bookBackground} alt={"asd"} />
+        <div className={classes.book_description_popup}>
+          <p>Year:{year}</p>
+          <p>Pages:{book_pages}</p>
+        </div>
+        <div className={classes.book_info}>
+          <p>Title:{title}</p>
+          <p>
+            {BsVectorPen}Author:{author}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BooksCard;
