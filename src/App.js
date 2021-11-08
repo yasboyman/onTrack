8de508325f34dfe8af58bf319be175d4318bc1aa
Y <y@Ys-MacBook-Air.local>
2021-11-08 00:25:43 +0000

    pagination now working and local storage added with custom react hook :)

--- src/App.js
+++ src/App.js
@@ -2,47 +2,19 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import BooksCard from "./components/BooksCard";
 import ReactPaginate from "react-paginate";
-import ScrollUp from "./components/ScrollUp";
-import Pagination from "./components/Pagination";
+import useLocalStorage from "./components/useLocalStorage";
+// import ScrollUp from "./components/ScrollUp";
 
 const App = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useLocalStorage("page", "1");
   const [SelectedItemsPerPage, setItemsPerPage] = useState(20);
   const [count, setCount] = useState("");
-  const [searchValue, setSearchValue] = useState("");
-  const [filters, setFitlers] = useState([]);
+  // const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useLocalStorage("name", "");
 
   const url = "http://nyx.vima.ekt.gr:3000/api/books/";
-
-  // const getBooks = async () => {
-  //   // const location = window.location.hostname;
-  //   console.log("GETTING BOOKS.....");
-  //   const settings = {
-  //     method: "POST",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       page: currentPage,
-  //       itemsPerPage: SelectedItemsPerPage,
-  //       filters: filters,
-  //     }),
-  //   };
-  //   try {
-  //     const fetchResponse = await fetch(`${url}`, settings);
-  //     console.log("SETTINGS", settings);
-  //     const result = await fetchResponse.json();
-  //     setIsLoading(false);
-  //     console.log("RESUTTTTTT", result);
-  //     setCount(Math.ceil(result.count));
-  //     return setData([result]);
-  //   } catch (e) {
-  //     return e;
-  //   }
-  // };
 
   useEffect(() => {
     // setIsLoading(true);
@@ -58,7 +30,7 @@ const App = () => {
         body: JSON.stringify({
           page: currentPage,
           itemsPerPage: SelectedItemsPerPage,
-          filters: filters,
+          // filters: filters,
         }),
       };
       try {
@@ -68,7 +40,7 @@ const App = () => {
         setIsLoading(false);
         console.log("RESUTTTTTT", result.books);
         console.log("countttt", result.count);
-        setCount(Math.ceil(result.count));
+        setCount(Math.ceil(result.count / result.books.length));
         return setData(result.books);
       } catch (e) {
         return e;
@@ -83,60 +55,17 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - SelectedItemsPerPage;
   const currentData = data && data.slice(indexOfFirstPost, indexOfLastPost);
   const lengthOfData = currentData && currentData.length;
-
-  console.log("currentData", currentData);
+  const totalPages = Math.ceil(count / data.length);
 
   //change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-  // console.log(data);
+  // const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const handlePageClick = (event) => {
     const newOffset = event.selected + 1;
     console.log("SELECTED::::", event.selected + 1);
     return setCurrentPage(newOffset);
-
-    // const clickedValue = e.target.innerHTML;
-    //
-    // setCurrentPage(clickedValue);
-    // getBooks();
   };
 
-  // const filterBySearch = (data) =>
-  //   searchValue == "" ||
-  //   (data[0].books.title.toLowerCase().includes(searchValue.toLowerCase()) &&
-  //     data);
-
-  // const handleSearchSubmit = () => {
-  //   setFitlers([searchValue]);
-  //   console.log("POSTTTTTT");
-  //   const getfitler = async () => {
-  //     // const location = window.location.hostname;
-  //     console.log("GETTING BOOKS.....");
-  //     const settings = {
-  //       method: "POST",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         filters: filters,
-  //       }),
-  //     };
-  //     try {
-  //       const fetchResponse = await fetch(`${url}`, settings);
-  //       const result = await fetchResponse.json();
-  //       setIsLoading(false);
-  //       console.log("RESUTTTTTT", result);
-  //       setCount(Math.ceil(result.count));
-  //       return setData([result]);
-  //     } catch (e) {
-  //       return e;
-  //     }
-  //   };
-
-  // getfitler();
-  // };
   console.log("CURRENT PAGE", currentPage);
   return (
     <div className="App">
@@ -149,18 +78,12 @@ const App = () => {
         />
         {/*<button onClick={handleSearchSubmit}>Submit</button>*/}
       </label>
-      <ScrollUp />
+
       {!isLoading && (
         <>
-          {/*<Pagination*/}
-          {/*  postPerPage={SelectedItemsPerPage}*/}
-          {/*  totalCount={count}*/}
-          {/*  paginate={paginate}*/}
-          {/*/>*/}
-
           <div className={"pagnationContainer"}>
             <ReactPaginate
-              pageCount={count / data.length}
+              pageCount={count}
               pageRange={2}
               forcePage={currentPage - 1}
               marginPagesDisplayed={2}
@@ -179,19 +102,7 @@ const App = () => {
 
       <div className={"books_parent_container"}>
         {data &&
-          data.map((item) => {
-            // console.log(item);
-            return (
-              <BooksCard
-                // id={item.id}
-                key={item.id}
-                title={item.book_title}
-                author={item.book_author}
-                book_pages={item.book_pages}
-                year={item.book_publication_year}
-              />
-            );
-          })}
+          data.map((item) => <BooksCard key={item.id} data={{ ...item }} />)}
       </div>
     </div>
   );
--- src/components/BooksCard/index.js
+++ src/components/BooksCard/index.js
@@ -3,19 +3,19 @@ import bookBackground from "../../assets/booksCard/index.png";
 import classes from "./index.module.css";
 import { BsVectorPen } from "react-icons/bs";
 
-const BooksCard = ({ title, author, book_pages, year }) => {
+const BooksCard = ({ title, author, book_pages, year, data }) => {
   return (
     <>
       <div className={classes.books_container}>
         <img src={bookBackground} alt={"asd"} />
         <div className={classes.book_description_popup}>
-          <p>Year:{year}</p>
-          <p>Pages:{book_pages}</p>
+          <p>Year:{data.book_publication_year}</p>
+          <p>Pages:{data.book_pages}</p>
         </div>
         <div className={classes.book_info}>
-          <p>Title:{title}</p>
+          <p>Title:{data.book_title}</p>
           <p>
-            {BsVectorPen}Author:{author}
+            {BsVectorPen}Author:{data.book_author}
           </p>
         </div>
       </div>
